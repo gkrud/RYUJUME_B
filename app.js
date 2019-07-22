ec1878cb281d8a51ea182f1521dd3f5783f80918
gkrud <gkrud4513@naver.com>
2019-07-22 09:30:06 +0900

    mongoose connect error update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const api = require('./routes/api');
 const config = require('./config');
@@ -17,7 +18,7 @@ app.set('jwt-secret', config.secret);
 app.use('/api',api);
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.mongodbUri, {useMongoClient:true})
+mongoose.connect(config.mongodbUri, {useNewUrlParser:true})
 .then(()=> console.log('Successfully connected to mongodb'))
 .catch(e => console.error(e));
 
