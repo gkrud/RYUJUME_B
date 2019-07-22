bb5999b1fe9a29e3a767252a0e745cdcee931279
gkrud <gkrud4513@naver.com>
2019-07-22 09:26:42 +0900

    mongoose connect

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('jwt-secret', config.secret);
 
 app.use('/api',api);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.mongodbUri, {useMongoClient:true})
+.then(()=> console.log('Successfully connected to mongodb'))
+.catch(e => console.error(e));
 
 app.listen(port, () => {
     console.log(`Express server is running at ${port}`);
