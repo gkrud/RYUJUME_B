fc10f2687fcd2a90f2e269dcb33640ca3d0effa1
gkrud <gkrud4513@naver.com>
2019-07-26 10:05:16 +0900

    api/index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.use('/docs', require('./docs'));
+router.use('/auth',require('./auth'));
+router.use('/myPage',require('./myPage'));
+router.use('/main',require('./main'));
 
 module.exports = router;
